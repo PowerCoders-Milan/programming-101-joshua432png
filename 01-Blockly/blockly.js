78d29900d2a7c99439f6948e6dc1437daae6879f
Joshua Udeh <udehasilo@gmail.com>
2021-02-18 12:22:37 +0100

    exe1

--- 01-Blockly/blockly.js
+++ 01-Blockly/blockly.js
@@ -1,1 +1,14 @@
 //Insert level 9 code below
+while (notDone()) {
+    if (isPathForward()) {
+      moveForward();
+    } else {
+      if (isPathLeft()) {
+        turnLeft();
+      } else {
+        if (isPathRight()) {
+          turnRight();
+        }
+      }
+    }
+  }
