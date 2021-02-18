390069b6df9957d3cebc1d162a6be741ee50899f
Joshua Udeh <udehasilo@gmail.com>
2021-02-18 18:56:35 +0100

    exercise 3

--- 03-Calendar-Calculator/calendar_calculator.js
+++ 03-Calendar-Calculator/calendar_calculator.js
@@ -2,5 +2,61 @@
 /*
 here
 */
+/* Specify the month a the day of the month in two variables
+the variable of the month will contain a name which will be the name of the month
+the second variable will contain a number which is the day of the month
+we need to first calculate the days of all the month before the name of the month
+calculate the requested number by adding the day
+print.
 
-//Insert your code below
+Examples:
+Variable = April
+Variable = 4
+Days before = 31 + 28 + 31 = 90
+Result = 90 + 4 = 94
+print
+
+//Insert your code below//
+var month, Day, Result;
+
+
+month = 'february';
+Day = 31;
+
+if (month == 'January') {
+  Result = 0 + Day;
+}
+if (month == 'february') {
+  Result = 31 + Day;
+}
+if (month == 'march') {
+  Result = 62 + Day;
+}
+if (month == 'April') {
+  Result = 92 + Day;
+}
+if (month == 'May') {
+  Result = 123 + Day;
+}
+if (month == 'June') {
+  Result = 153 + Day;
+}
+if (month == 'July') {
+  Result = 184 + Day;
+}
+if (month == 'August') {
+  Result = 215 + Day;
+}
+if (month == 'September') {
+  Result = 245 + Day;
+}
+if (month == 'October') {
+  Result = 276 + Day;
+}
+if (month == 'November') {
+  Result = 306 + Day;
+}
+if (month == 'December') {
+  Result = 337 + Day;
+}
+window.alert(Result);*/
