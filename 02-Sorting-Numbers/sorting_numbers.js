d7a11d2efc49eca3c93b15a8c6d6d1c8d217e640
Joshua Udeh <udehasilo@gmail.com>
2021-02-18 14:32:46 +0100

    exercise 2

--- 02-Sorting-Numbers/sorting_numbers.js
+++ 02-Sorting-Numbers/sorting_numbers.js
@@ -3,4 +3,27 @@
 here
 */
 
+/*create a list of numbers
+sort the numbers by ascending order
+print the numbers
+
+
 //insert your code below
+var num;
+
+function listsGetSortCompare(type, direction) {
+  var compareFuncs = {
+    "NUMERIC": function(a, b) {
+        return Number(a) - Number(b); },
+    "TEXT": function(a, b) {
+        return a.toString() > b.toString() ? 1 : -1; },
+    "IGNORE_CASE": function(a, b) {
+        return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1; },
+  };
+  var compare = compareFuncs[type];
+  return function(a, b) { return compare(a, b) * direction; }
+}
+
+
+num = [5, 4, 3];
+window.alert(num.slice().sort(listsGetSortCompare("NUMERIC", 1)));
